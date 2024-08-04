b6140cc108bb3a1ff3cd8635f02378e9a2c887eb
asm-2000 <ashim.baral@bajratechnologies.com>
2024-08-04 14:56:36 +0545

    feat: implement customer cart schema structure

--- Models/Cart.js
+++ Models/Cart.js
@@ -1,7 +1,7 @@
 const { Sequelize, DataTypes } = require("sequelize");
 const bcrypt = require("bcrypt");
 
-// Set up Sequelize to connect to PostgreSQL
+// Set up Sequelize to connect to PostgreSQL.
 
 const sequelize = new Sequelize(
   "postgres://postgres:ashim50@localhost:5432/flamingfork",
@@ -10,6 +10,36 @@ const sequelize = new Sequelize(
   }
 );
 
-const Cart = sequelize.define();
+// Define customercart schema structure.
+
+const Cart = sequelize.define(
+  "customercart",
+  {
+    cartid: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    customerid: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    cartitems: {
+      type: DataTypes.JSONB,
+      allowNull: false,
+      get() {
+        const value = this.getDataValue("orderitem");
+        return value ? value : [];
+      },
+      set(value) {
+        this.setDataValue("orderitem", value);
+      },
+    },
+  },
+  {
+    freezeTableName: true,
+    timestamps: false,
+  }
+);
 
 module.exports = Cart;
