e0b750adf9862687320e489eaa7bcd9a68f4ad29
asm-2000 <ashim.baral@bajratechnologies.com>
2024-08-04 14:48:53 +0545

    feat: implement customer order schema structure

--- Models/Order.js
+++ Models/Order.js
@@ -10,6 +10,44 @@ const sequelize = new Sequelize(
   }
 );
 
-const Order = sequelize.define();
+// Define customerorder schema structure
+
+const Order = sequelize.define(
+  "customerorder",
+  {
+    orderid: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    customerid: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    customercontact: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    orderstatus: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    orderitems: {
+      type: DataTypes.JSONB,
+      allowNull: false,
+      get() {
+        const value = this.getDataValue("orderitem");
+        return value ? value : [];
+      },
+      set(value) {
+        this.setDataValue("orderitem", value);
+      },
+    },
+  },
+  {
+    freezeTableName: true,
+    timestamps: false,
+  }
+);
 
 module.exports = Order;
