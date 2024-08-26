8d1fa8eda787225873f404b9fbb030f66b824685
asm-2000 <ashim.baral@bajratechnologies.com>
2024-08-26 21:19:42 +0545

    feat: implement handlers to send orders having different statuses

--- Routes/orderRoute.js
+++ Routes/orderRoute.js
@@ -2,15 +2,60 @@ const express = require("express");
 const CustomerOrder = require("../Models/Order");
 const OrderItem = require("../Models/OrderItem");
 const auth = require("../Middleware/authentication");
-const { where } = require("sequelize");
 const router = express.Router();
 
-// Handler to get all active orders (Admin side).
+// Handler to get all orders (Admin side).
 
-router.get("/activeOrders", auth, async (req, res, next) => {
+router.get("/allOrders", auth, async (req, res, next) => {
+  const currentDate = new Date();
+  const year = currentDate.getFullYear();
+  const month = String(currentDate.getMonth() + 1).padStart(2, "0"); 
+  const day = String(currentDate.getDate()).padStart(2, "0");
+  const todayDate = `${year}/${month}/${day}`;
+  try {
+    const allOrders = await CustomerOrder.findAll({
+      raw: true,
+    });
+    const todaysAllOrders = allOrders.map(order => {if(order.orderdate.includes(todayDate))
+    {
+      return order;
+    }
+    })
+    if (allOrders) {
+      const orders = await Promise.all(
+        allOrders.map(async (order) => {
+          const alItemsInOrder = await OrderItem.findAll({
+            where: { orderid: order.orderid },
+          });
+
+          return {
+            orderid: order.orderid,
+            customercontact: order.customercontact,
+            customeraddress: order.customeraddress,
+            orderstatus: order.orderstatus,
+            orderitems: alItemsInOrder,
+            orderdate: order.orderdate,
+          };
+        })
+      );
+      if (orders.length > 0) {
+        res.status(200).json({ orders });
+      } else {
+        res.status(200).json({ message: "No orders to show!" });
+      }
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Handler to get all Placed orders (Admin side).
+
+router.get("/placedOrders", auth, async (req, res, next) => {
   try {
     const allPlacedOrders = await CustomerOrder.findAll({
-      where: { orderstatus: "placed" },raw:true
+      where: { orderstatus: "Placed" },
+      raw: true,
     });
     if (allPlacedOrders) {
       const orders = await Promise.all(
@@ -23,15 +68,16 @@ router.get("/activeOrders", auth, async (req, res, next) => {
             orderid: placedOrder.orderid,
             customercontact: placedOrder.customercontact,
             customeraddress: placedOrder.customeraddress,
-            items: alItemsInPlacedOrder,
-            orderdate: placedOrder.orderdate
+            orderitems: alItemsInPlacedOrder,
+            orderstatus: placedOrder.orderstatus,
+            orderdate: placedOrder.orderdate,
           };
         })
       );
       if (orders.length > 0) {
         res.status(200).json({ orders });
       } else {
-        res.status(200).json({ message: "No active orders!" });
+        res.status(200).json({ message: "No orders received to Show!" });
       }
     }
   } catch (error) {
@@ -39,33 +85,107 @@ router.get("/activeOrders", auth, async (req, res, next) => {
   }
 });
 
-// Handler to get all completed orders (Admin side).
+// Handler to get all orders being prepared (Admin side).
 
-router.get("/completedOrders", auth, async (req, res, next) => {
+router.get("/beingPreparedOrders", auth, async (req, res, next) => {
   try {
-    const allCompletedOrders = await CustomerOrder.findAll({
-      where: { orderstatus: "completed" },raw:true
+    const allBeingPreparedOrders = await CustomerOrder.findAll({
+      where: { orderstatus: "Being Prepared" },
+      raw: true,
     });
-    if (allCompletedOrders) {
+    if (allBeingPreparedOrders) {
       const orders = await Promise.all(
-        allCompletedOrders.map(async (completedOrder) => {
-          const alItemsInCompletedOrder = await OrderItem.findAll({
-            where: { orderid: completedOrder.orderid },
+        allBeingPreparedOrders.map(async (beingPreparedOrder) => {
+          const alItemsInBeingPreparedOrder = await OrderItem.findAll({
+            where: { orderid: beingPreparedOrder.orderid },
           });
 
           return {
-            orderid: completedOrder.orderid,
-            customercontact: completedOrder.customercontact,
-            customeraddress: completedOrder.Customeraddress,
-            items: alItemsInCompletedOrder,
-            orderdate: completedOrder.orderdate
+            orderid: beingPreparedOrder.orderid,
+            customercontact: beingPreparedOrder.customercontact,
+            customeraddress: beingPreparedOrder.customeraddress,
+            orderitems: alItemsInBeingPreparedOrder,
+            orderstatus: beingPreparedOrder.orderstatus,
+            orderdate: beingPreparedOrder.orderdate,
           };
         })
       );
       if (orders.length > 0) {
         res.status(200).json({ orders });
       } else {
-        res.status(200).json({ message: "No completed orders!" });
+        res.status(200).json({ message: "No orders being prepared!" });
+      }
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Handler to get all orders being delivered (Admin side).
+
+router.get("/beingDeliveredOrders", auth, async (req, res, next) => {
+  try {
+    const allBeingDeliveredOrders = await CustomerOrder.findAll({
+      where: { orderstatus: "Being Delivered" },
+      raw: true,
+    });
+    if (allBeingDeliveredOrders) {
+      const orders = await Promise.all(
+        allBeingDeliveredOrders.map(async (beingDeliveredOrder) => {
+          const alItemsInBeingDeliveredOrder = await OrderItem.findAll({
+            where: { orderid: beingDeliveredOrder.orderid },
+          });
+
+          return {
+            orderid: beingDeliveredOrder.orderid,
+            customercontact: beingDeliveredOrder.customercontact,
+            customeraddress: beingDeliveredOrder.customeraddress,
+            orderitems: alItemsInBeingDeliveredOrder,
+            orderstatus: beingDeliveredOrder.orderstatus,
+            orderdate: beingDeliveredOrder.orderdate,
+          };
+        })
+      );
+      if (orders.length > 0) {
+        res.status(200).json({ orders });
+      } else {
+        res.status(200).json({ message: "No orders being delivered!" });
+      }
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Handler to get all delivered orders (Admin side).
+
+router.get("/deliveredOrders", auth, async (req, res, next) => {
+  try {
+    const allDeliveredOrders = await CustomerOrder.findAll({
+      where: { orderstatus: "Delivered" },
+      raw: true,
+    });
+    if (allDeliveredOrders) {
+      const orders = await Promise.all(
+        allDeliveredOrders.map(async (deliveredOrder) => {
+          const alItemsInDeliveredOrder = await OrderItem.findAll({
+            where: { orderid: deliveredOrder.orderid },
+          });
+
+          return {
+            orderid: deliveredOrder.orderid,
+            customercontact: deliveredOrder.customercontact,
+            customeraddress: deliveredOrder.customeraddress,
+            orderitems: alItemsInDeliveredOrder,
+            orderstatus: deliveredOrder.orderstatus,
+            orderdate: deliveredOrder.orderdate,
+          };
+        })
+      );
+      if (orders.length > 0) {
+        res.status(200).json({ orders });
+      } else {
+        res.status(200).json({ message: "No delivered orders to show!" });
       }
     }
   } catch (error) {
@@ -78,7 +198,8 @@ router.get("/completedOrders", auth, async (req, res, next) => {
 router.get("/cancelledOrders", auth, async (req, res, next) => {
   try {
     const allCancelledOrders = await CustomerOrder.findAll({
-      where: { orderstatus: "cancelled" },raw:true
+      where: { orderstatus: "Cancelled" },
+      raw: true,
     });
     if (allCancelledOrders) {
       const orders = await Promise.all(
@@ -90,16 +211,17 @@ router.get("/cancelledOrders", auth, async (req, res, next) => {
           return {
             orderid: cancelledOrder.orderid,
             customercontact: cancelledOrder.customercontact,
-            customeraddress: cancelledOrder.Customeraddress,
-            items: alItemsInCancelledOrder,
-            orderdate: cancelledOrder.orderdate
+            customeraddress: cancelledOrder.customeraddress,
+            orderitems: alItemsInCancelledOrder,
+            orderstatus: cancelledOrder.orderstatus,
+            orderdate: cancelledOrder.orderdate,
           };
         })
       );
       if (orders.length > 0) {
         res.status(200).json({ orders });
       } else {
-        res.status(200).json({ message: "No cancelled orders!" });
+        res.status(200).json({ message: "No cancelled orders to show!" });
       }
     }
   } catch (error) {
@@ -110,27 +232,35 @@ router.get("/cancelledOrders", auth, async (req, res, next) => {
 // Handler to change the status of the order.
 
 router.put("/changeOrderStatus", auth, async (req, res, next) => {
-  const { orderId, customerId, customerContact, customerAddress, orderStatus, orderDate } =
-    req.body;
-    console.log(req.body);
+  const {
+    orderId,
+    customerId,
+    customerContact,
+    customerAddress,
+    orderStatus,
+    orderDate,
+  } = req.body;
+  console.log(req.body);
   const customerOrder = {
     orderid: orderId,
     customerid: customerId,
     customercontact: customerContact,
     customeraddress: customerAddress,
     orderstatus: orderStatus,
-    orderdate: orderDate
+    orderdate: orderDate,
   };
-    try {
-      const updated = await CustomerOrder.update(customerOrder, {
-        where: { customerid: customerOrder.customerid, orderid: customerOrder.orderid },
-      });
-      if (updated[0] > 0) {
-        res.status(200).json({ message: "Status updated sucessfully!" });
-      } else res.status(404).json({ message: "Order not found!" });
-    } catch (error) {
-      next(error);
-    }
+  try {
+    const updated = await CustomerOrder.update(customerOrder, {
+      where: {
+        orderid: customerOrder.orderid,
+      },
+    });
+    if (updated[0] > 0) {
+      res.status(200).json({ message: "Status updated sucessfully!" });
+    } else res.status(404).json({ message: "Order not found!" });
+  } catch (error) {
+    next(error);
+  }
 });
 
 //Handler to place a customer order.
@@ -142,7 +272,7 @@ router.post("/placeCustomerOrder", auth, async (req, res, next) => {
     customerAddress,
     orderStatus,
     orderItems,
-    orderDate
+    orderDate,
   } = req.body;
   try {
     const order = await CustomerOrder.create({
@@ -150,7 +280,7 @@ router.post("/placeCustomerOrder", auth, async (req, res, next) => {
       customercontact: customerContact,
       customeraddress: customerAddress,
       orderstatus: orderStatus,
-      orderdate: orderDate
+      orderdate: orderDate,
     });
     orderItems.map(async (orderItem) => {
       await OrderItem.create({
@@ -172,14 +302,16 @@ router.get("/customerOrders/:customerId", auth, async (req, res, next) => {
   const { customerId } = req.params;
   try {
     const allCustomerOrders = await CustomerOrder.findAll({
-      where: { customerid: customerId }, raw:true,
+      where: { customerid: customerId },
+      raw: true,
     });
     console.log(allCustomerOrders);
     if (allCustomerOrders) {
       const orders = await Promise.all(
         allCustomerOrders.map(async (customerOrder) => {
           const allItemsInCustomerOrder = await OrderItem.findAll({
-            where: { orderid: customerOrder.orderid },raw:true
+            where: { orderid: customerOrder.orderid },
+            raw: true,
           });
           return {
             orderid: customerOrder.orderid,
@@ -188,12 +320,12 @@ router.get("/customerOrders/:customerId", auth, async (req, res, next) => {
             customeraddress: customerOrder.customeraddress,
             orderstatus: customerOrder.orderstatus,
             orderitems: allItemsInCustomerOrder,
-            orderdate: customerOrder.orderdate
+            orderdate: customerOrder.orderdate,
           };
         })
       );
       if (orders.length > 0) {
-        res.status(200).json({ customerOrders:orders });
+        res.status(200).json({ customerOrders: orders });
       } else {
         res.status(200).json({ message: "No orders!" });
       }
@@ -206,7 +338,7 @@ router.get("/customerOrders/:customerId", auth, async (req, res, next) => {
 // Internal error handler.
 
 router.use((error, req, res, next) => {
-  console.log(error.message)
+  console.log(error.message);
   res.status(500).json({ message: error.message });
 });
 
